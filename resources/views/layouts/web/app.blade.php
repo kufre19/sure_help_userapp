f58657166cd729325ba78c4f3e3022026113b66f
Emmanuel Kufre <whitemaxwell5@gmial.com>
2023-04-17 12:51:45 +0100

    init ci cd

---
+++ resources/views/layouts/web/app.blade.php
@@ -0,0 +1,318 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{env('APP_NAME')}}</title>
+
+    <link href="{{asset('custom/assets/fonts.googleapis.com/csse965.css?family=Roboto:300,400%7CSource+Sans+Pro:700')}}" rel="stylesheet">
+
+    <link type="text/css" rel="stylesheet" href="{{asset('custom/assets/css/bootstrap.min.css')}}" />
+
+    <link type="text/css" rel="stylesheet" href="{{asset('custom/assets/css/owl.carousel.css')}}" />
+    <link type="text/css" rel="stylesheet" href="{{asset('custom/assets/css/owl.theme.default.css')}}" />
+
+    <link rel="stylesheet" href="{{asset('custom/assets/css/font-awesome.min.css')}}">
+
+    <link type="text/css" rel="stylesheet" href="{{asset('custom/assets/css/style.css')}}" />
+
+</head>
+
+<body>
+
+    <header id="home">
+
+        <nav id="main-navbar">
+            <div class="container">
+                <div class="navbar-header">
+
+                    <div class="navbar-brand">
+                        <a class="logo" href="index-2.html"><img src="{{asset('surehelp_logo.png')}}" alt="logo"></a>
+                    </div>
+
+
+                    <button class="navbar-toggle-btn">
+                        <i class="fa fa-bars"></i>
+                    </button>
+
+
+                    <button class="search-toggle-btn">
+                        <i class="fa fa-search"></i>
+                    </button>
+
+                </div>
+
+                <div class="navbar-search">
+                    <button class="search-btn"><i class="fa fa-search"></i></button>
+                    <div class="search-form">
+                        <form>
+                            <input class="input" type="text" name="search" placeholder="Search">
+                        </form>
+                    </div>
+                </div>
+
+
+                <ul class="navbar-menu nav navbar-nav navbar-right">
+                    <li><a href="index-2.html">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    
+                    {{-- <li class="has-dropdown"><a href="#">Blog</a>
+                        <ul class="dropdown">
+                            <li><a href="blog.html">Blog Page</a></li>
+                            <li><a href="single-blog.html">Single Blog</a></li>
+                        </ul>
+                    </li> --}}
+                    <li><a href="#">Contact</a></li>
+                </ul>
+
+            </div>
+        </nav>
+
+
+        <div id="home-owl" class="owl-carousel owl-theme">
+
+            <div class="home-item">
+
+                <div class="section-bg" style="background-image: url({{asset('custom/assets/img/background-1.jpg')}});"></div>
+
+
+                <div class="home">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-8">
+                                <div class="home-content">
+                                    <h1>Save The Children</h1>
+                                    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                                        eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                    <a href="#" class="primary-button">View Causes</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div class="home-item">
+
+                <div class="section-bg" style="background-image: url({{asset('custom/assets/img/background-2.jpg')}});"></div>
+
+
+                <div class="home">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-8">
+                                <div class="home-content">
+                                    <h1>Become A Partner</h1>
+                                    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                                        eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                    <a href="#" class="primary-button">Join Us Now!</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </header>
+
+
+    @yield('main-content')
+
+
+  
+
+
+    {{-- <div id="blog" class="section">
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="section-title text-center">
+                        <h2 class="title">Our Blog</h2>
+                        <p class="sub-title">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                </div>
+
+
+                <div class="col-md-4">
+                    <div class="article">
+                        <div class="article-img">
+                            <a href="single-blog.html">
+                                <img src="img/post-1.jpg" alt="">
+                            </a>
+                        </div>
+                        <div class="article-content">
+                            <h3 class="article-title"><a href="single-blog.html">Possit nostro aeterno eu vis, ut cum
+                                    quem reque</a></h3>
+                            <ul class="article-meta">
+                                <li>12 November 2018</li>
+                                <li>By John doe</li>
+                                <li>0 Comments</li>
+                            </ul>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua.</p>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-md-4">
+                    <div class="article">
+                        <div class="article-img">
+                            <a href="single-blog.html">
+                                <img src="img/post-2.jpg" alt="">
+                            </a>
+                        </div>
+                        <div class="article-content">
+                            <h3 class="article-title"><a href="single-blog.html">Vix fuisset tibique facilisis cu.
+                                    Justo accusata ius at</a></h3>
+                            <ul class="article-meta">
+                                <li>12 November 2018</li>
+                                <li>By John doe</li>
+                                <li>0 Comments</li>
+                            </ul>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua.</p>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-md-4">
+                    <div class="article">
+                        <div class="article-img">
+                            <a href="single-blog.html">
+                                <img src="img/post-3.jpg" alt="">
+                            </a>
+                        </div>
+                        <div class="article-content">
+                            <h3 class="article-title"><a href="single-blog.html">Possit nostro aeterno eu vis, ut cum
+                                    quem reque</a></h3>
+                            <ul class="article-meta">
+                                <li>12 November 2018</li>
+                                <li>By John doe</li>
+                                <li>0 Comments</li>
+                            </ul>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua.</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div> --}}
+
+
+    <footer id="footer" class="section">
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-md-4">
+                    <div class="footer">
+                        <div class="footer-logo">
+                            <a class="logo" href="#"><img src="img/logo.png" alt=""></a>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.</p>
+                        <ul class="footer-contact">
+                            <li><i class="fa fa-map-marker"></i> address</li>
+                            <li><i class="fa fa-phone"></i>phone</li>
+                           
+                        </ul>
+                    </div>
+                </div>
+
+
+                
+
+
+                <div class="col-md-4">
+                    <div class="footer">
+                        <h3 class="footer-title">Newsletter</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.
+                        </p>
+                        <form class="footer-newsletter">
+                            <input class="input" type="email" placeholder="Enter your email">
+                            <button class="primary-button">Subscribe</button>
+                        </form>
+                        <ul class="footer-social">
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div id="footer-bottom" class="row">
+                <div class="col-md-6 col-md-push-6">
+                    <ul class="footer-nav">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        {{-- <li><a href="#">Causes</a></li>
+                        <li><a href="#">Events</a></li>
+                        <li><a href="#">Blog</a></li> --}}
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-6 col-md-pull-6">
+                    <div class="footer-copyright">
+                        <span>
+                            Copyright &copy;
+                           
+                            <script>
+                                document.write(new Date().getFullYear());
+                            </script> All rights reserved | {{env('APP_NAME')}}| {{date("Y")}}</a>
+                        </span>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </footer>
+
+
+    <script src="{{asset('custom/assets/js/jquery.min.js')}}"></script>
+    <script src="{{asset('custom/assets/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('custom/assets/js/owl.carousel.min.js')}}"></script>
+    <script src="{{asset('custom/assets/js/jquery.stellar.min.js')}}"></script>
+    <script src="{{asset('custom/assets/js/main.js')}}"></script>
+
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-23581568-13');
+    </script>
+    <script defer src="https://static.cloudflareinsights.com/beacon.min.js/v2b4487d741ca48dcbadcaf954e159fc61680799950996"
+        integrity="sha512-D/jdE0CypeVxFadTejKGTzmwyV10c1pxZk/AqjJuZbaJwGMyNHY3q/mTPWqMUnFACfCTunhZUVcd4cV78dK1pQ=="
+        data-cf-beacon='{"rayId":"7b8b6e4b6af176db","version":"2023.3.0","b":1,"token":"cd0b4b3a733644fc843ef0b185f98241","si":100}'
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ resources/views/web/home.blade.php
@@ -0,0 +1,207 @@
+@extends('layouts.web.app')
+
+
+@section('main-content')
+    <div id="about" class="section">
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-md-5">
+                    <div class="section-title">
+                        <h2 class="title">Welcome to {{ env('APP_NAME') }}</h2>
+                        <p class="sub-title">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                    <div class="about-content">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <a href="#" class="primary-button">Read More</a>
+                    </div>
+                </div>
+
+
+                <div class="col-md-offset-1 col-md-6">
+                    <a href="#" class="about-video">
+                        <i class="play-icon fa fa-play"></i>
+                        <img src="{{ asset('custom/assets/img/about.jpg') }}" alt="">
+                    </a>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+    <div id="numbers" class="section">
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-md-3 col-sm-6">
+                    <div class="number">
+                        <i class="fa fa-smile-o"></i>
+                        <h3>47k</h3>
+                        <span>Donors</span>
+                    </div>
+                </div>
+
+
+                <div class="col-md-3 col-sm-6">
+                    <div class="number">
+                        <i class="fa fa-heartbeat"></i>
+                        <h3>154K</h3>
+                        <span>Children Saved</span>
+                    </div>
+                </div>
+
+
+                <div class="col-md-3 col-sm-6">
+                    <div class="number">
+                        <i class="fa fa-money"></i>
+                        <h3>785K</h3>
+                        <span>Donated</span>
+                    </div>
+                </div>
+
+
+                <div class="col-md-3 col-sm-6">
+                    <div class="number">
+                        <i class="fa fa-handshake-o"></i>
+                        <h3>357</h3>
+                        <span>Partner</span>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+
+    <div id="cta" class="section">
+
+        <div class="section-bg" style="background-image: url(img/background-1.jpg);" data-stellar-background-ratio="0.5">
+        </div>
+
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-md-offset-2 col-md-8">
+                    <div class="cta-content text-center">
+                        <h1>Become A Partner1>
+                            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua.</p>
+                            <a href="#" class="primary-button">Join Us Now!</a>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+
+
+    <div id="testimonial" class="section">
+
+        <div class="section-bg" style="background-image: url(img/background-2.jpg);" data-stellar-background-ratio="0.5">
+        </div>
+
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-md-12">
+                    <div id="testimonial-owl" class="owl-carousel owl-theme">
+
+                        <div class="testimonial">
+                            <div class="testimonial-meta">
+                                <div class="testimonial-img">
+                                    <img src="{{ asset('custom/assets/img/avatar-1.jpg') }}" alt="">
+                                </div>
+                                <h3>John Doe</h3>
+                                <span>Partner</span>
+                            </div>
+                            <div class="testimonial-quote">
+                                <blockquote>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                                        incididunt ut labore et dolore magna aliqua.</p>
+                                </blockquote>
+                            </div>
+                        </div>
+
+
+                        <div class="testimonial">
+                            <div class="testimonial-meta">
+                                <div class="testimonial-img">
+                                    <img src="{{ asset('custom/assets/img/avatar-2.jpg') }}" alt="">
+                                </div>
+                                <h3>John Doe</h3>
+                                <span>Partner</span>
+                            </div>
+                            <div class="testimonial-quote">
+                                <blockquote>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                                        incididunt ut labore et dolore magna aliqua.</p>
+                                </blockquote>
+                            </div>
+                        </div>
+
+
+                        <div class="testimonial">
+                            <div class="testimonial-meta">
+                                <div class="testimonial-img">
+                                    <img src="{{ asset('custom/assets/img/avatar-1.jpg') }}" alt="">
+                                </div>
+                                <h3>John Doe</h3>
+                                <span>Partner</span>
+                            </div>
+                            <div class="testimonial-quote">
+                                <blockquote>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                                        incididunt ut labore et dolore magna aliqua.</p>
+                                </blockquote>
+                            </div>
+                        </div>
+
+
+                        <div class="testimonial">
+                            <div class="testimonial-meta">
+                                <div class="testimonial-img">
+                                    <img src="{{ asset('custom/assets/img/avatar-2.jpg') }}" alt="">
+                                </div>
+                                <h3>John Doe</h3>
+                                <span>Partner</span>
+                            </div>
+                            <div class="testimonial-quote">
+                                <blockquote>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                                        incididunt ut labore et dolore magna aliqua.</p>
+                                </blockquote>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+@endsection
