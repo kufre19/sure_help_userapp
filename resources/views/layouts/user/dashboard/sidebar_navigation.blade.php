d901675309a420ffe8260deaeaf8b3a9494350cf
Emmanuel <whitemaxwell5@gmail.com>
2023-05-30 18:01:35 +0100

    user dashboard frontend materials complete

--- resources/views/layouts/user/dashboard/sidebar_navigation.blade.php
+++ resources/views/layouts/user/dashboard/sidebar_navigation.blade.php
@@ -3,63 +3,33 @@
         <ul class="nav" id="main-menu">
 
             <li>
-                <a class="active-menu waves-effect waves-dark" href="index.html"><i
-                        class="fa fa-dashboard"></i> Dashboard</a>
+                <a class="active-menu waves-effect waves-dark" href="index.html"><i class="fa fa-dashboard"></i>
+                    Dashboard</a>
             </li>
             <li>
-                <a href="ui-elements.html" class="waves-effect waves-dark"><i class="fa fa-desktop"></i> UI
-                    Elements</a>
+                <a href="ui-elements.html" class="waves-effect waves-dark"><i class="fa fa-desktop"></i>
+                    Inbox
+                </a>
             </li>
             <li>
-                <a href="chart.html" class="waves-effect waves-dark"><i class="fa fa-bar-chart-o"></i>
-                    Charts</a>
-            </li>
-            <li>
-                <a href="tab-panel.html" class="waves-effect waves-dark"><i class="fa fa-qrcode"></i> Tabs &
-                    Panels</a>
-            </li>
-
-            <li>
-                <a href="table.html" class="waves-effect waves-dark"><i class="fa fa-table"></i> Responsive
-                    Tables</a>
-            </li>
-            <li>
-                <a href="form.html" class="waves-effect waves-dark"><i class="fa fa-edit"></i> Forms </a>
-            </li>
-
-
-            <li>
-                <a href="#" class="waves-effect waves-dark"><i class="fa fa-sitemap"></i> Multi-Level
-                    Dropdown<span class="fa arrow"></span></a>
+                <a href="#" class="waves-effect waves-dark"><i class="fa fa-sitemap"></i> 
+                    My Requests
+                    <span class="fa arrow"></span></a>
                 <ul class="nav nav-second-level">
                     <li>
-                        <a href="#">Second Level Link</a>
+                        <a href="#">Approved</a>
                     </li>
                     <li>
-                        <a href="#">Second Level Link</a>
+                        <a href="#">Rejected</a>
                     </li>
-                    <li>
-                        <a href="#">Second Level Link<span class="fa arrow"></span></a>
-                        <ul class="nav nav-third-level">
-                            <li>
-                                <a href="#">Third Level Link</a>
-                            </li>
-                            <li>
-                                <a href="#">Third Level Link</a>
-                            </li>
-                            <li>
-                                <a href="#">Third Level Link</a>
-                            </li>
-
-                        </ul>
-
-                    </li>
+                    
                 </ul>
             </li>
             <li>
-                <a href="empty.html" class="waves-effect waves-dark"><i class="fa fa-fw fa-file"></i> Empty
-                    Page</a>
+                <a href="tab-panel.html" class="waves-effect waves-dark"><i class="fa fa-qrcode"></i> 
+                    Free Store</a>
             </li>
+
         </ul>
 
     </div>
