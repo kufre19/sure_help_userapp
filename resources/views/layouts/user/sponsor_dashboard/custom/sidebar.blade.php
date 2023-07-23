74499a0b0a5e5a9769a18a4bb638033da8b7213e
Emmanuel kufre <whitemaxwell5@gmail.com>
2023-07-23 21:35:05 +0100

    same as last

--- resources/views/layouts/user/sponsor_dashboard/custom/sidebar.blade.php
+++ resources/views/layouts/user/sponsor_dashboard/custom/sidebar.blade.php
@@ -1,109 +1,109 @@
   <!-- Sidebar -->
   <ul class="navbar-nav bg-gradient-surehelp sidebar sidebar-dark accordion" id="accordionSidebar">
 
-    <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{url('/dashboard')}}">
-        <div class="sidebar-brand-icon rotate-n-15">
-            {{-- <i class="fas fa-laugh-wink"></i>
+      <!-- Sidebar - Brand -->
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ url('/dashboard') }}">
+          <div class="sidebar-brand-icon rotate-n-15">
+              {{-- <i class="fas fa-laugh-wink"></i>
              --}}
-             
-        </div>
-        <div class="sidebar-brand-text mx-3">{{ env('APP_NAME') }} <sup>{{Auth::user()->fullname}}</sup></div>
-    </a>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider my-0">
+          </div>
+          <div class="sidebar-brand-text mx-3">{{ env('APP_NAME') }} <sup>{{ Auth::user()->fullname }}</sup></div>
+      </a>
 
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item">
-        <a class="nav-link" href="{{url('/dashboard')}}">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
-    </li>
+      <!-- Divider -->
+      <hr class="sidebar-divider my-0">
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
+      <!-- Nav Item - Dashboard -->
+      <li class="nav-item">
+          <a class="nav-link" href="{{ url('/dashboard') }}">
+              <i class="fas fa-fw fa-tachometer-alt"></i>
+              <span>Dashboard</span></a>
+      </li>
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Navigations
-    </div>
+      <!-- Divider -->
+      <hr class="sidebar-divider">
 
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-            aria-expanded="true" aria-controls="collapseTwo">
-            <i class="fas fa-fw fa-rss"></i>
-            <span>News Feed</span>
-        </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Feeds section:</h6>
-                <a class="collapse-item" href="{{url("sponsor/dashboard/news-feeds")}}">See News Feed</a>
-            
-            </div>
-        </div>
-    </li>
+      <!-- Heading -->
+      <div class="sidebar-heading">
+          Navigations
+      </div>
 
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseThree"
-            aria-expanded="true" aria-controls="collapseThree">
-            <i class="fas fa-fw fa-envelope"></i>
-            <span>Inbox</span>
-        </a>
-        <div id="collapseThree" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Inbox section:</h6>
-                <a class="collapse-item" href="{{url('sponsor/dashboard/inbox/messages')}}">Check Inbox</a>
-                
-            </div>
-        </div>
-    </li>
+      <!-- Nav Item - Pages Collapse Menu -->
+      <li class="nav-item">
+          <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+              aria-expanded="true" aria-controls="collapseTwo">
+              <i class="fas fa-fw fa-rss"></i>
+              <span>News Feed</span>
+          </a>
+          <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+              <div class="bg-white py-2 collapse-inner rounded">
+                  <h6 class="collapse-header">Feeds section:</h6>
+                  <a class="collapse-item" href="{{ url('sponsor/dashboard/news-feeds') }}">See News Feed</a>
 
-     <!-- Nav Item - Pages Collapse Menu -->
-     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseFive"
-            aria-expanded="true" aria-controls="collapseFive">
-            <i class="fas fa-fw fa-wallet"></i>
-            <span>Payment Methods</span>
-        </a>
-        <div id="collapseFive" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Payment Methods section:</h6>
-                <a class="collapse-item" href="{{url('sponsor/dashboard')}}">Payment Methods</a>
-            
-            </div>
-        </div>
-    </li>
+              </div>
+          </div>
+      </li>
 
-     {{-- <!-- Nav Item - Pages Collapse Menu -->
-     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseFour"
-            aria-expanded="true" aria-controls="collapseFour">
-            <i class="fas fa-fw fa-store"></i>
-            <span>Free Store</span>
-        </a>
-        <div id="collapseFour" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Free Store section:</h6>
-                <a class="collapse-item" href="{{url('/store')}}">Free Store</a>
-                <a class="collapse-item" href="{{url('dashboard/store/wish-list')}}">My Wish List</a>
-                <a class="collapse-item" href="#">Rejected</a>
-            </div>
-        </div>
-    </li> --}}
+      <!-- Nav Item - Pages Collapse Menu -->
+      <li class="nav-item">
+          <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseThree"
+              aria-expanded="true" aria-controls="collapseThree">
+              <i class="fas fa-fw fa-envelope"></i>
+              <span>Inbox</span>
+          </a>
+          <div id="collapseThree" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+              <div class="bg-white py-2 collapse-inner rounded">
+                  <h6 class="collapse-header">Inbox section:</h6>
+                  <a class="collapse-item" href="{{ url('sponsor/dashboard/inbox/messages') }}">Check Inbox</a>
 
-    
+              </div>
+          </div>
+      </li>
+      <!-- Nav Item - Pages Collapse Menu -->
+      <li class="nav-item">
+          <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseFour"
+              aria-expanded="true" aria-controls="collapseFour">
+              <i class="fas fa-fw fa-quote-left"></i>
+              <span>Testimonial</span>
+          </a>
+          <div id="collapseFour" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+              <div class="bg-white py-2 collapse-inner rounded">
+                  <h6 class="collapse-header">Testimonial section:</h6>
+                  <a class="collapse-item" href="{{ url('sponsor/dashboard') }}">See Testimonials</a>
+               
+              </div>
+          </div>
+      </li>
 
-  
+      <!-- Nav Item - Pages Collapse Menu -->
+      <li class="nav-item">
+          <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseFive"
+              aria-expanded="true" aria-controls="collapseFive">
+              <i class="fas fa-fw fa-wallet"></i>
+              <span>Payment Methods</span>
+          </a>
+          <div id="collapseFive" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+              <div class="bg-white py-2 collapse-inner rounded">
+                  <h6 class="collapse-header">Payment Methods section:</h6>
+                  <a class="collapse-item" href="{{ url('sponsor/dashboard') }}">Payment Methods</a>
 
-    <hr class="sidebar-divider d-none d-md-block">
+              </div>
+          </div>
+      </li>
 
-    <!-- Sidebar Toggler (Sidebar) -->
-    <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"><i class="arrow-left"></i></button>
-    </div>
 
-</ul>
-<!-- End of Sidebar -->
+
+
+
+
+
+      <hr class="sidebar-divider d-none d-md-block">
+
+      <!-- Sidebar Toggler (Sidebar) -->
+      <div class="text-center d-none d-md-inline">
+          <button class="rounded-circle border-0" id="sidebarToggle"><i class="arrow-left"></i></button>
+      </div>
+
+  </ul>
+  <!-- End of Sidebar -->
