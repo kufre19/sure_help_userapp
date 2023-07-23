d3eaef2a968821833c21948b54f38d558c49e2f9
Emmanuel kufre <whitemaxwell5@gmail.com>
2023-07-23 21:20:43 +0100

    rm wishkist from sponsor sd bar

--- resources/views/layouts/user/sponsor_dashboard/custom/sidebar.blade.php
+++ resources/views/layouts/user/sponsor_dashboard/custom/sidebar.blade.php
@@ -78,7 +78,7 @@
     </li>
 
      <!-- Nav Item - Pages Collapse Menu -->
-     <li class="nav-item">
+     {{-- <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseFour"
             aria-expanded="true" aria-controls="collapseFour">
             <i class="fas fa-fw fa-store"></i>
@@ -92,7 +92,7 @@
                 {{-- <a class="collapse-item" href="#">Rejected</a> --}}
             </div>
         </div>
-    </li>
+    </li> --}}
 
     
 
