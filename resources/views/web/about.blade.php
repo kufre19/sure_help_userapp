42e56a00c0033e33cfe42d6a3b7e57d37fa1ec1e
Emmanuel kufre <whitemaxwell5@gmail.com>
2023-10-21 12:11:04 +0100

    fix syncing error

--- resources/views/web/about.blade.php
+++ resources/views/web/about.blade.php
@@ -1,11 +1,178 @@
 @extends('layouts.web.app')
 
 @section('main-content')
-    <div id="about" class="">
+   
+
+    <section class="bg-02-a">
         <div class="container">
-            <h2>About Us</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget dictum leo. Donec consequat consectetur arcu, non suscipit odio bibendum in. Nulla facilisi. Maecenas ullamcorper, mi eget hendrerit gravida, nibh felis bibendum dolor, eget venenatis nunc velit vel nisi. Sed sollicitudin enim eu risus auctor, vel consectetur sapien ultrices. Nulla at lectus ipsum. Nullam nec maximus velit. Aliquam erat volutpat. Nunc id purus vitae magna viverra feugiat. Fusce interdum sapien vel lectus rhoncus ullamcorper.</p>
-            <p>Donec ac dui libero. Suspendisse potenti. Sed faucibus, dui vel suscipit venenatis, quam tellus suscipit nibh, vel vestibulum ex nisi non velit. Sed interdum diam vel ligula faucibus, eget laoreet est bibendum. Duis quis nulla porttitor, finibus nisi at, rutrum mauris. Sed a suscipit mauris. Nullam et mauris nisi. Duis imperdiet euismod diam a lobortis. Vivamus vestibulum aliquam leo, eu sagittis velit porta sed. Aenean mollis semper sapien, at cursus arcu vulputate sit amet. In in ligula justo. Mauris dignissim, quam eu faucibus dignissim, ex quam lacinia odio, vitae consectetur felis sapien sed dui.</p>
+            <div class="row">
+                <div class="col-12">
+                    <div class="_head_01">
+                        <h2>About Us</h2>
+                        <p>Home<i class="fas fa-angle-right"></i><span>About Us</span></p>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
+    </section>
+
+    <section class="se-001">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-12">
+                    <div class="_Ol_er_qw">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis voluptatibus neque, assumenda
+                            maxime. Eaque libero unde corrupti deleniti maxime ratione doloremque suscipit perferendis
+                            aperiam labore debitis atque odit neque, possimus, aspernatur dicta nobis recusandae numquam
+                            provident porro, quam suscipit quibusdam. Commodi eum, optio quo.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis voluptatibus neque, assumenda
+                            maxime. Eaque libero unde corrupti deleniti maxime ratione doloremque suscipit perferendis
+                            aperiam labore debitis atque odit neque, possimus, aspernatur dicta nobis recusandae numquam
+                            provident porro, quam suscipit quibusdam. Commodi eum, optio quo.</p>
+                    </div>
+                </div>
+
+                <div class="col-lg-6 col-md-6 col-12">
+                    <div class="_Ol_er_qw yu">
+                        <img src="{{ asset('assets/web/images/team/11.jpg')}}">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- ====================== section started====================== -->
+
+    <section class="bg-03">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+                    <div class="_lk_bg_cd">
+                        <i class="fal fa-school"></i>
+                        <div class="counting" data-count="128">128</div>
+                        <h5>Primary Schools</h5>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+                    <div class="_lk_bg_cd">
+                        <i class="fal fa-hospitals"></i>
+                        <div class="counting" data-count="300">300</div>
+                        <h5>Hospitals</h5>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+                    <div class="_lk_bg_cd">
+                        <i class="fal fa-hands-helping"></i>
+                        <div class="counting" data-count="250">250</div>
+                        <h5>Volunteers</h5>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+                    <div class="_lk_bg_cd">
+                        <i class="fal fa-trophy"></i>
+                        <div class="counting" data-count="400">400</div>
+                        <h5>Winning Awards</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- ====================== Team Started started====================== -->
+
+    <section class="team">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="heading">
+                        <h2>OUR TEAM</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime totam quo, ducimus aliquid
+                            quisquam
+                            minima perspiciatis repellendus, minus tenetur reiciendis quis? Consequatur perferendis
+                            deleniti, rerum
+                            delectus consectetur modi praesentium deserunt.</p>
+                    </div>
+                </div>
+
+                <div class="col-12">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                            <div class="team-card">
+                                <div class="image-team">
+                                    <img src="assets/images/team/1.jpg">
+                                </div>
+                                <div class="team-content">
+                                    <h3>John</h3>
+                                    <p>Web Developer</p>
+                                    <ol>
+                                        <li><i class="fab fa-facebook-f"></i></li>
+                                        <li><i class="fab fa-instagram"></i></li>
+                                        <li><i class="fab fa-linkedin-in"></i></li>
+                                        <li><i class="fab fa-pinterest-p"></i></li>
+                                    </ol>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                            <div class="team-card">
+                                <div class="image-team">
+                                    <img src="assets/images/team/2.jpg">
+                                </div>
+                                <div class="team-content">
+                                    <h3>Anderson john</h3>
+                                    <p>Web Designer</p>
+                                    <ol>
+                                        <li><i class="fab fa-facebook-f"></i></li>
+                                        <li><i class="fab fa-instagram"></i></li>
+                                        <li><i class="fab fa-linkedin-in"></i></li>
+                                        <li><i class="fab fa-pinterest-p"></i></li>
+                                    </ol>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12"">
+                            <div class=" team-card">
+                                <div class="image-team">
+                                    <img src="assets/images/team/3.jpg">
+                                </div>
+                                <div class="team-content">
+                                    <h3>Sarah Se</h3>
+                                    <p>Web Designer</p>
+                                    <ol>
+                                        <li><i class="fab fa-facebook-f"></i></li>
+                                        <li><i class="fab fa-instagram"></i></li>
+                                        <li><i class="fab fa-linkedin-in"></i></li>
+                                        <li><i class="fab fa-pinterest-p"></i></li>
+                                    </ol>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12"">
+                            <div class=" team-card">
+                                <div class="image-team">
+                                    <img src="assets/images/team/4.jpg">
+                                </div>
+                                <div class="team-content">
+                                    <h3>Williams</h3>
+                                    <p>English </p>
+                                    <ol>
+                                        <li><i class="fab fa-facebook-f"></i></li>
+                                        <li><i class="fab fa-instagram"></i></li>
+                                        <li><i class="fab fa-linkedin-in"></i></li>
+                                        <li><i class="fab fa-pinterest-p"></i></li>
+                                    </ol>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
--- resources/views/web/contact.blade.php
+++ resources/views/web/contact.blade.php
@@ -1,37 +1,129 @@
 @extends('layouts.web.app')
 
 @section('main-content')
-    <div id="contact" class="section">
-        <div class="container">
-            <h2>Contact Us</h2>
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="contact-info">
-                        <i class="fa fa-phone"></i>
-                        <h4>Phone</h4>
-                        <p>1-800-555-1234</p>
-                    </div>
-                    <div class="contact-info">
-                        <i class="fa fa-envelope"></i>
-                        <h4>Email</h4>
-                        <p>info@example.com</p>
-                    </div>
-                    <div class="contact-info">
-                        <i class="fas fa-map-marker-alt"></i>
-                        <h4>Address</h4>
-                        <p>123 Main St, Anytown USA</p>
-                    </div>
-                </div>
-                <div class="col-md-6 border-solid">
-                  <form class="footer-newsletter">
-                    <input class="input " type="email" placeholder="Enter your email">
-                    <br> 
-                    <textarea class="input textarea pd-2" >Enter Your Message </textarea>
+    <!-- ======================header started====================== -->
 
-                    <button class="primary-button">Message Us</button>
-                </form>
+   
+
+    <section class="bg-02-a">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="_head_01">
+                        <h2>Contact Us</h2>
+                        <p>Home<i class="fas fa-angle-right"></i><span>Contact Us</span></p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
+
+    <section class="_pl_rt">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="_lo_we_ds">
+                        <div class="row">
+                            <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+                                <div class="_ol_we_as">
+                                    <ol>
+                                        <li><i class="far fa-location"></i>
+                                            <h3>Location</h3>4353 White Pine Lane
+                                        </li>
+                                    </ol>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+                                <div class="_ol_we_as">
+                                    <ol>
+                                        <li><i class="far fa-envelope"></i>
+                                            <h3>Email</h3>sales@smarteyeapps.com
+                                        </li>
+                                    </ol>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+                                <div class="_ol_we_as">
+                                    <ol>
+                                        <li><i class="fas fa-mobile-alt"></i>
+                                            <h3>Phone</h3>540-715-1353
+                                        </li>
+                                    </ol>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- ====================== Form started====================== -->
+
+    <section class="my-pla">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="heading">
+                        <h2 style="color:#fff;">Get In Touch</h2>
+                        <p style="color:#fff;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                            eiusmod
+                            tempor incididunt</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-6 col-12">
+                    <div class="form-group">
+                        <label for="name">Name <small>*</small></label>
+                        <input id="name" name="name" class="form-control _ge_de_ol" type="text"
+                            placeholder="Enter Name" required="" aria-required="true">
+                    </div>
+                </div>
+
+                <div class="col-lg-6 col-12">
+                    <div class="form-group">
+                        <label for="name">Email <small>*</small></label>
+                        <input id="email" name="email" class="form-control _ge_de_ol" type="text"
+                            placeholder="Enter Email" required="" aria-required="true">
+                    </div>
+                </div>
+
+                <div class="col-lg-6 col-12">
+                    <div class="form-group">
+                        <label for="name">Subject <small>*</small></label>
+                        <input id="subject" name="subject" class="form-control _ge_de_ol" type="text"
+                            placeholder="Enter Subject" required="" aria-required="true">
+                    </div>
+                </div>
+
+                <div class="col-lg-6 col-12">
+                    <div class="form-group">
+                        <label for="name">Phone <small>*</small></label>
+                        <input id="phone" name="phone" class="form-control _ge_de_ol" type="text"
+                            placeholder="phone number" required="" aria-required="true">
+                    </div>
+                </div>
+
+                <div class="col-12">
+                    <div class="form-group">
+                        <label for="message">Message</label>
+                        <textarea id="message" name="message" class="form-control required" rows="5" placeholder="Enter Message"
+                            aria-required="true"></textarea>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    {{-- <!-- ====================== Map started====================== -->
+
+    <section class="mab-01">
+        <iframe style="width:100%"
+            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d249759.19784092825!2d79.10145254589841!3d12.009924873581818!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1448883859107"
+            height="450" frameborder="0" allowfullscreen=""></iframe>
+    </section> --}}
 @endsection
