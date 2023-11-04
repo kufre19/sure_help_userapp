157b598b608e238be13fa325a20deaa0a98b59d6
Emmanuel kufre <whitemaxwell5@gmail.com>
2023-11-04 11:17:35 +0100

    s a l

--- resources/views/user/dashboard/alerts/all.blade.php
+++ resources/views/user/dashboard/alerts/all.blade.php
@@ -1,5 +1,11 @@
 @if (session('success'))
-<div id="disapear-alert" class="alert alert-success" role="alert" style="display: none;">
+<div id="disapear-alert" class="alert alert-success" role="alert">
    {{session('success')}}
+</div>
+@endif
+
+@if (session('error'))
+<div id="disapear-alert" class="alert alert-error" role="alert">
+   {{session('error')}}
 </div>
 @endif
