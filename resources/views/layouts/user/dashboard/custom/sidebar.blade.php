ce944f0c0325333a85e081c8908a8a8c2ab4f547
Emmanuel kufre <whitemaxwell5@gmail.com>
2023-11-04 11:28:23 +0100

    remove request from side bar

--- resources/views/layouts/user/dashboard/custom/sidebar.blade.php
+++ resources/views/layouts/user/dashboard/custom/sidebar.blade.php
@@ -32,7 +32,7 @@
     </div>
 
     <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item">
+    {{-- <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
             aria-expanded="true" aria-controls="collapseTwo">
             <i class="fas fa-fw fa-clipboard"></i>
@@ -45,7 +45,7 @@
                 <a class="collapse-item" href="{{url("/dashboard")}}">My Requests</a>
             </div>
         </div>
-    </li>
+    </li> --}}
 
     <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
