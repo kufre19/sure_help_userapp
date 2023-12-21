8063741f48ad91e21cac824fb76dc0ebac18800b
Emmanuel kufre <whitemaxwell5@gmail.com>
2023-12-21 02:55:40 +0100

    fixing routing

--- resources/views/layouts/user/sponsor_dashboard/custom/sidebar.blade.php
+++ resources/views/layouts/user/sponsor_dashboard/custom/sidebar.blade.php
@@ -16,7 +16,7 @@
 
       <!-- Nav Item - Dashboard -->
       <li class="nav-item">
-          <a class="nav-link" href="{{ url('/dashboard') }}">
+          <a class="nav-link" href="{{ url('sponsor/dashboard') }}">
               <i class="fas fa-fw fa-tachometer-alt"></i>
               <span>Dashboard</span></a>
       </li>
